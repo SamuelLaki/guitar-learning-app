da742297fa47621e361ea2e09f160fc9d0da3197
Sarah Nasser <sarahnasser@Sarahs-MacBook-Air-7.local>
2025-04-20 14:47:06 -0400

    Linked each chord button on Learn page, excluding the 'Learn in Order' and 'Chord Reading Basics' buttons, to their corresponding pages, which are unfinished so far and only contain the name of each chord and a bit of the UI design for now. I also added folders containing all the images used in this website and all the chord audios.

--- server.py
+++ server.py
@@ -2,6 +2,12 @@ from flask import Flask, request, jsonify, render_template, redirect, url_for, j
 
 app = Flask(__name__)
 
+chord_items = [
+    {"id": 1,
+     "chord": "A",
+     "image": "https://www.til.co/_next/image?url=%2Fguitar%2Fchords%2FAmajor.png&w=640&q=75"}
+]
+
 
 @app.route('/')
 def homepage():
@@ -14,6 +20,10 @@ def learn():
 @app.route('/quiz')
 def quiz():
     return render_template('quiz.html')
+
+@app.route('/learn/<chord_name>')
+def chord_detail(chord_name):
+    return render_template('chord_detail.html', chord=chord_name)
 
 if __name__ == '__main__':
     app.run(debug=True, port=5001)
