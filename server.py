fa986fb2cb5eb1b27874c88ff5a07f37f679fd11
Tesfalem <te2320@columbia.edu>
2025-05-05 22:00:28 -0400

    created a static folder for chord and images to prevent display inconsistency

--- server.py
+++ server.py
@@ -10,50 +10,50 @@ chord_items = [
     {
         "id": 1,
         "name": "A",
-        "image": "https://github.com/SarahNasser576/Sarah_Tesfalem_Samuel_UI-Design_Final-Project/blob/main/images/A_chord_image.jpg?raw=true",
-        "audio": "https://github.com/SarahNasser576/Sarah_Tesfalem_Samuel_UI-Design_Final-Project/blob/main/chord_audios/A_chord_audio.mp3?raw=true"
+        "image": "/static/images/A_chord_image.jpg",
+        "audio": "/static/chord_audios/A_chord_audio.mp3"
     },
     {
         "id": 2,
         "name": "D",
-        "image": "https://github.com/SarahNasser576/Sarah_Tesfalem_Samuel_UI-Design_Final-Project/blob/main/images/D_chord_image.jpg?raw=true",
-        "audio": "https://github.com/SarahNasser576/Sarah_Tesfalem_Samuel_UI-Design_Final-Project/blob/main/chord_audios/D_chord_audio.mp3?raw=true"
+        "image": "/static/images/D_chord_image.jpg",
+        "audio": "/static/chord_audios/D_chord_audio.mp3"
     },
     {
         "id": 3,
         "name": "E",
-        "image": "https://github.com/SarahNasser576/Sarah_Tesfalem_Samuel_UI-Design_Final-Project/blob/main/images/E_chord_image.jpg?raw=true",
-        "audio": "https://github.com/SarahNasser576/Sarah_Tesfalem_Samuel_UI-Design_Final-Project/blob/main/chord_audios/E_chord_audio.mp3?raw=true"
+        "image": "/static/images/E_chord_image.jpg",
+        "audio": "/static/chord_audios/E_chord_audio.mp3"
     },
     {
         "id": 4,
         "name": "Am",
-        "image": "https://github.com/SarahNasser576/Sarah_Tesfalem_Samuel_UI-Design_Final-Project/blob/main/images/Am_chord_image.jpg?raw=true",
-        "audio": "https://github.com/SarahNasser576/Sarah_Tesfalem_Samuel_UI-Design_Final-Project/blob/main/chord_audios/Am_chord_audio.mp3?raw=true"
+        "image": "/static/images/Am_chord_image.jpg",
+        "audio": "/static/chord_audios/Am_chord_audio.mp3"
     },
     {
         "id": 5,
         "name": "Em",
-        "image": "https://github.com/SarahNasser576/Sarah_Tesfalem_Samuel_UI-Design_Final-Project/blob/main/images/Em_chord_image.jpg?raw=true",
-        "audio": "https://github.com/SarahNasser576/Sarah_Tesfalem_Samuel_UI-Design_Final-Project/blob/main/chord_audios/Em_chord_audio.mp3?raw=true"
+        "image": "/static/images/Em_chord_image.jpg",
+        "audio": "/static/chord_audios/Em_chord_audio.mp3"
     },
     {
         "id": 6,
         "name": "Dm",
-        "image": "https://github.com/SarahNasser576/Sarah_Tesfalem_Samuel_UI-Design_Final-Project/blob/main/images/Dm_chord_image.jpg?raw=true",
-        "audio": "https://github.com/SarahNasser576/Sarah_Tesfalem_Samuel_UI-Design_Final-Project/blob/main/chord_audios/Dm_chord_audio.mp3?raw=true"
+        "image": "/static/images/Dm_chord_image.jpg",
+        "audio": "/static/chord_audios/Dm_chord_audio.mp3"
     },
     {
         "id": 7,
         "name": "C",
-        "image": "https://github.com/SarahNasser576/Sarah_Tesfalem_Samuel_UI-Design_Final-Project/blob/main/images/C_chord_image.jpg?raw=true",
-        "audio": "https://github.com/SarahNasser576/Sarah_Tesfalem_Samuel_UI-Design_Final-Project/blob/main/chord_audios/C_chord_audio.mp3?raw=true"
+        "image": "/static/images/C_chord_image.jpg",
+        "audio": "/static/chord_audios/C_chord_audio.mp3"
     },
     {
         "id": 8,
         "name": "G",
-        "image": "https://github.com/SarahNasser576/Sarah_Tesfalem_Samuel_UI-Design_Final-Project/blob/main/images/G_chord_image.jpg?raw=true",
-        "audio": "https://github.com/SarahNasser576/Sarah_Tesfalem_Samuel_UI-Design_Final-Project/blob/main/chord_audios/G_chord_audio.mp3?raw=true"
+        "image": "/static/images/G_chord_image.jpg",
+        "audio": "/static/chord_audios/G_chord_audio.mp3"
     }
 ]
 
